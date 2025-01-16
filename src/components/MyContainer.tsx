3769bd5224f33068e78d5e16f85429c5d0e1bf00
OskarSan <oskar.sankila@gmail.com>
2025-01-16 11:42:42 +0200

    task 4

--- src/components/MyContainer.tsx
+++ src/components/MyContainer.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import MyList from './MyList';
+
+
+import MyList , {TItem} from './MyList';
+import { useState } from 'react';
 
 type MyContainerProps = {
 
@@ -7,14 +10,34 @@ type MyContainerProps = {
 
 const MyContainer: React.FC<MyContainerProps> = () => {
     const header = "List Header";
-    const items = [
+    const [items, setItems] = useState<TItem[]> ([
         { id: "1", text: "First item" },
         { id: "2", text: "Second item" },
         { id: "3", text: "Third item" }
-    ];
+    ]);
 
-    return (  
-      <MyList header={header} items={items} />
+    const [newItemText, setNewItemText] = useState<string>("");
+
+    const handleAddItem = () => {
+        const newItem: TItem = {
+            id: (items.length + 1).toString(),
+            text: newItemText
+        };
+        setItems([...items, newItem]);
+  
+    }
+
+
+    return ( 
+      <div>
+        <MyList header={header} items={items} />
+        <textarea 
+            value={newItemText}
+            onChange={(e) => setNewItemText(e.target.value)}
+        ></textarea>
+        <button onClick={handleAddItem}>Add Item</button>
+      </div>   
+      
   );
 }
 
--- src/components/MyList.tsx
+++ src/components/MyList.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-type TItem = {
+export type TItem = {
     id: string;
     text: string;
 };
