48e97a46fefca5f048d89cd9d82e67f9d70e39ef
OskarSan <oskar.sankila@gmail.com>
2025-01-16 11:09:00 +0200

    task2

---
+++ src/components/MyContainer.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import MyList from './MyList';
+
+type MyContainerProps = {
+
+}
+
+const MyContainer: React.FC<MyContainerProps> = () => {
+  return (
+  
+      <MyList />
+   
+  );
+}
+
+export default MyContainer;
---
+++ src/components/MyList.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MyList: React.FC = () => {
+    return (
+        <ul>
+            <li>First item</li>
+            <li>Second item</li>
+            <li>Third item</li>
+        </ul>
+    )
+};
+
+export default MyList;
